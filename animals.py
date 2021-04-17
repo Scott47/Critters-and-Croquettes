d2be692a74d76b62bd677d3f8158e67dc29c7ac0
Scott47 <scottsilver47@gmail.com>
2021-04-17 13:32:17 -0500

    initial commit

---
+++ animals.py
@@ -0,0 +1,184 @@
+from datetime import date
+
+class Llama:
+
+    def __init__(self, location):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.location = location
+        self.walking = True
+
+class Goat:
+
+    def __init__(self, location):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.location = location
+        self.walking = True
+
+class Ostrich:
+
+    def __init__(self, location):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.location = location
+        self.walking = True
+
+class Alpaca:
+
+    def __init__(self, location):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.location = location
+        self.walking = True
+
+class Elk:
+
+    def __init__(self, location):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.location = location
+        self.walking = True
+
+class Cobra:
+
+    def __init__(self, location):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.location = location
+        self.slithering = True
+
+class Ratsnake:
+
+    def __init__(self, location):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.location = location
+        self.slithering = True
+
+class Indigo:
+
+    def __init__(self, location):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.location = location
+        self.slithering = True
+
+class Brownsnake:
+
+    def __init__(self, location):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.location = location
+        self.slithering = True
+
+class Copperhead:
+
+    def __init__(self, location):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.location = location
+        self.slithering = True
+
+class Narwhal:
+
+    def __init__(self, location):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.location = location
+        self.swimming = True
+
+class Manatee:
+
+    def __init__(self, location):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.location = location
+        self.swimming = True
+
+class Whale:
+
+    def __init__(self, location):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.location = location
+        self.swimming = True
+
+class Dolphin:
+
+    def __init__(self, location):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.location = location
+        self.swimming = True
+
+class Shark:
+
+    def __init__(self, location):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.date_added = date.today()
+        self.location = location
+        self.swimming = True
+
+miss_fuzz = Llama('petting area')
+miss_buzz = Goat('petting area')
+miss_guzz = Ostrich('petting area')
+miss_cuzz = Alpaca('petting area')
+miss_huzz = Elk('petting area')
+print(miss_buzz)
+
+miss_hiss = Cobra('glass tank')
+miss_siss = Ratsnake('glass tank')
+miss_fiss = Copperhead('glass tank')
+miss_piss = Brownsnake('glass tank')
+miss_ciss = Indigo('glass tank')
+print(miss_ciss)
+
+miss_blow = Narwhal('the pond')
+miss_blow.name = "mystical"
+print(miss_blow)
